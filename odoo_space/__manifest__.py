6bc617e1172a7e5a2ea941d2c7f54a733b83d7e7
panv-odoo <panv@odoo.com>
2024-02-12 18:57:53 +0530

    [IMP] odoo_space: make adjustment into the form and tree view
    Implemented Tree and Form view for the charging station and also implemented

--- odoo_space/__manifest__.py
+++ odoo_space/__manifest__.py
@@ -12,9 +12,9 @@
     'data':[
         'security/ir.model.access.csv',
 
-        'views/odoospace_menus.xml',
         'views/user_property_views.xml',
-        'views/station_property_view.xml'
+        'views/station_property_view.xml',
+        'views/odoospace_menus.xml',
         
     ],
     'license': 'LGPL-3',
--- odoo_space/models/station_property.py
+++ odoo_space/models/station_property.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from odoo import fields,models
+from dateutil.relativedelta import relativedelta
 
 class Station(models.Model):
     _name = "station.property"
     # never user underscore in name
     _description = "EV Charging Station"
 
-    code = fields.Char('Code',required=True)
-    capacity = fields.Integer('Charging Capacity (kWh)')
-    availability = fields.Boolean('is Available',required=True)
-    remaining_time = fields.Datetime('Remaining Time')
+    name = fields.Char('Code',default='CS-',required=True,copy=False)
+    capacity = fields.Float('Charging Capacity (kWh)',copy=False)
+    availability = fields.Boolean('is Available',required=True,default=True)
+    remaining_time = fields.Datetime('Remaining Time',copy=False,default=lambda self: fields.datetime.now()+relativedelta(minutes=30))
+    active = fields.Boolean(default=True)
     
